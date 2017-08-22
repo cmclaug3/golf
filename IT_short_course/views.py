69b17a16d60010512bbefb27a2833cc61df5226e
Corey McLaughlin <mclaughlin848@gmail.com>
2017-08-22 17:03:51 -0600

    example of my problem

--- IT_short_course/views.py
+++ IT_short_course/views.py
@@ -7,10 +7,18 @@ from IT_short_course.models import Round, Player, Match
 
 
 def home(request):
+
+	yea = []
+	test = Round.objects.all()
+	for score in test:
+		yea.append(score.to_par)
+	total_average_score = (sum(yea)) / test.count
+
 	context = {
 		'all_rounds': Round.objects.all(),
 		'all_players': Player.objects.all(),
-		'all_matches': Match.objects.all(), 
+		'all_matches': Match.objects.all(),
+		'total_average_score': total_average_score
 	}
 	return render(request, 'home.html', context)
 
