7cec821a0960581c0f2e1924b8c83aac2a7dc21d
Corey McLaughlin <mclaughlin848@gmail.com>
2017-08-22 21:52:42 -0600

    completed basic single player view/template/url

--- IT_short_course/urls.py
+++ IT_short_course/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from IT_short_course.views import home
+from IT_short_course.views import home, single_player
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^player/(?P<player_id>[0-9]+)$', single_player, name='single_player'),
 ]
--- IT_short_course/views.py
+++ IT_short_course/views.py
@@ -107,8 +107,11 @@ def home(request):
 	nine_avg = sum(ninth) / float(len(ninth))
 
 
+	# total holes
+	total_holes = len(all_rounds) * 9
+
 	# hardest to easiest holes
-	
+
 
 
 	context = {
@@ -131,8 +134,144 @@ def home(request):
 		'six_avg': six_avg,
 		'seven_avg': seven_avg,
 		'eight_avg': eight_avg,
-		'nine_avg': nine_avg
+		'nine_avg': nine_avg,
+		'total_holes': total_holes
 	}
 	return render(request, 'home.html', context)
+
+
+
+def single_player(request, player_id):
+
+	player = Player.objects.get(id=player_id)
+	player_rounds = player.round_set.all()
+
+	# player average score
+	yea = []
+	test = player_rounds
+	for score in test:
+		yea.append(score.to_par)
+	player_average_score = round((sum(yea)) / float(test.count()), 3)
+
+	# player average hole
+	holes = []
+	big = player_rounds
+	for hole_score in big:
+		holes.append(hole_score.average_hole_score())
+	player_average_hole = round((sum(holes)) / float(big.count()), 3)
+
+	# total aces 
+	aces = []
+	for ace in player_rounds:
+		aces.append(ace.ace_count())
+	player_aces = sum(aces)
+
+	# total birdies 
+	birdies = []
+	for birdie in player_rounds:
+		birdies.append(birdie.birdie_count())
+	player_birdies = sum(birdies)
+
+	# total pars 
+	pars = []
+	for par in player_rounds:
+		pars.append(par.par_count())
+	player_pars = sum(pars)
+
+	# total bogies 
+	bogies = []
+	for bogie in player_rounds:
+		bogies.append(bogie.bogie_count())
+	player_bogies = sum(bogies)
+
+	# total doubles 
+	doubles = []
+	for double in player_rounds:
+		doubles.append(double.double_count())
+	player_doubles = sum(doubles)
+
+	# total trips and over 
+	trips = []
+	for trip in player_rounds:
+		trips.append(trip.trip_and_over_count())
+	player_trips_and_above = sum(trips)
+
+	# average score by hole
+	first = []
+	second = []
+	third = []
+	fourth = []
+	fifth = []
+	sixth = []
+	seventh = []
+	eighth = []
+	ninth = []
+
+	for one_score in player_rounds:
+		first.append(one_score.one)
+	one_avg = sum(first) / float(len(first))
+
+	for two_score in player_rounds:
+		second.append(two_score.two)
+	two_avg = sum(second) / float(len(second))
+
+	for three_score in player_rounds:
+		third.append(three_score.three)
+	three_avg = sum(third) / float(len(third))
+
+	for four_score in player_rounds:
+		fourth.append(four_score.four)
+	four_avg = sum(fourth) / float(len(fourth))
+
+	for five_score in player_rounds:
+		fifth.append(five_score.five)
+	five_avg = sum(fifth) / float(len(fifth))
+
+	for six_score in player_rounds:
+		sixth.append(six_score.six)
+	six_avg = sum(sixth) / float(len(sixth))
+
+	for seven_score in player_rounds:
+		seventh.append(seven_score.seven)
+	seven_avg = sum(seventh) / float(len(seventh))
+
+	for eight_score in player_rounds:
+		eighth.append(eight_score.eight)
+	eight_avg = sum(eighth) / float(len(eighth))
+
+	for nine_score in player_rounds:
+		ninth.append(nine_score.nine)
+	nine_avg = sum(ninth) / float(len(ninth))
+
+	# player match record
+	
+
+
+	context = {
+		'player_name': player.name,
+		'player_rounds_amount': len(player_rounds),
+		'player_average_score': player_average_score,
+		'round_total': len(player_rounds) * 9,
+		'player_average_hole': player_average_hole,
+		'player_aces': player_aces,
+		'player_birdies': player_birdies,
+		'player_pars': player_pars,
+		'player_bogies': player_bogies,
+		'player_doubles': player_doubles,
+		'player_trips_and_above': player_trips_and_above,
+		'one_avg': one_avg,
+		'two_avg': two_avg,
+		'three_avg': three_avg,
+		'four_avg': four_avg,
+		'five_avg': five_avg,
+		'six_avg': six_avg,
+		'seven_avg': seven_avg,
+		'eight_avg': eight_avg,
+		'nine_avg': nine_avg,
+	}
+
+	return render(request, 'single_player.html', context)
+
+
 
 
